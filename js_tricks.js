be46e4a10b289ef12dc1e0c368a5a7d5088de672
Jatinokov <Enkhtaivan1206@gmail.com>
2021-07-28 14:48:47 -0700

    to uptodate

--- js_tricks.js
+++ js_tricks.js
@@ -37,7 +37,7 @@ function addList_items(type, items) {
     document.querySelector(list).insertAdjacentHTML("beforeend", dom);
 };
 
-/* array dotor-s hamgiin suulchiin element-g olood, unique id-g ni avh */
+/* array dotorh elementuuded davtagdahgui unique id uguh and hamgiin suulchiin element-g olood, unique id-g ni avh */
 var items, id;
 if (all_User_Data.data['inc'].length === 0) {
     id = 1;
@@ -45,3 +45,29 @@ if (all_User_Data.data['inc'].length === 0) {
     // got lastest item from array and add new item after it then put their id's in id
     id = all_User_Data.data['exp'][all_User_Data.data[type].length - 1].id + 1;
 };
+
+/* temdegt muriig buyu STRING-g TOO ruu shiljuuleh using parseInt() */
+var num = "100";
+parseInt(num);
+
+/* forEach ashiglaj value -g nemeh */
+var sum = 0;
+// adding the values to sum
+all_User_Data.data[type].forEach(function (el) {
+    sum = sum + el.value;
+})
+// putting the sum to total
+all_User_Data.total[type] = sum;
+
+/* map() ashiglaj elementiin id nuudiig stugluulsan array uusgeh */
+var items_id = all_User_Data.data[type].map(function (el) {
+    return el.id;
+});
+
+/* indexOf() ashiglaj bairlaliig item-n index -g oloh */
+var index = all_User_Data.data[type].indexOf(items_id);
+/* splice() ashiglaj ter oloson index -g array-s ustgah */
+
+if (index !== -1) {
+    all_User_Data.data[type].splice(index, 1)
+}
