b05736faf69f231763b3406ad78233afe0fe22df
Evank1206 <jamessain_66@yahoo.com>
2025-03-31 13:17:51 -0700

    added new examples to switch operator

--- switch/switch.js
+++ switch/switch.js
@@ -1,37 +1,89 @@
 /* 1 */
-console.log("....................................");
+let fruits = "banana";
 
-let nam = "z";
+switch (fruits) {
 
-switch (nam) {
-    case "x":
-        console.log("hi");
+    case "apple":
+        console.log("apple is fruit");
         break;
-    case "y":
-        console.log("hello");
+
+    case "banana":
+        console.log("The banana is fruit");
         break;
+
     default:
-        console.log("yeahh")
+        console.log("can't find OR some other message");
+
 }
 /* 2 */
 console.log("....................................");
 
-// let fruits = "banana";
+// find a matching number
+var num = 5;
 
-// switch (fruits) {
+switch (num) {
+  case 2:
+    console.log('yes This is number ' + num);
+    break;
+  case 3:
+    console.log('yes This is number ' + num);
+    break;
+  case 4:
+    console.log('yes This is number ' + num);
+    break;
+  case 5:
+    console.log('yes This is number ' + num);
+    break;
 
-//     case "apple":
-//         console.log("apple is fruit");
-//         break;
+  default:
+    console.log('can not find matching number ....');
+}
 
-//     case "banana":
-//         console.log("The banana is fruit");
-//         break;
+// recuruting
+var name, age, job, drivingStatus, experience, interest;
+    name = 'John';
+    age = 25;
+    job = 'driver';
+    drivingStatusYears = 12;
+    record = 'clean';
+    interest = 'like to drive';
 
-//     default:
-//         console.log("some other massage");
+// switch (true) {
+//   case job === 'cook' ||
+//     (interest === 'like to drive' &&
+//       age >= 21 &&
+//       drivingStatusYears >= 10 &&
+//       record === 'clean'):
+//     console.log(name + ' hired');
+//     break;
 
-// }
+//   default:
+//     console.log('still looking...');
+// };
+
+//with function
+var fun = function(){
+    switch (true) {
+     case job === 'cook' ||
+        (interest === 'like to drive' &&
+         age >= 21 &&
+          drivingStatusYears >= 10 &&
+          record === 'clean'):
+          return 1;
+        console.log(name + ' hired');
+     
+      break;
+  
+      default:
+      return 0;
+      console.log('still looking...');
+  };
+  
+  };
+  
+  var rfun = fun();
+  // console.log(rfun);
+  rfun !== 0 ? console.log("received") : console.log("NOT received");
 
 /* 3 */
 console.log("....................................");
--- text.js
+++ text.js
@@ -108,6 +108,35 @@ for (let i = listsName.length - 1; i > 0; i--) {
 
 
 
+https://evank1206.github.io/MediaQuary/#
+{/* <script> */}
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+// </script>
+
 
 
 
